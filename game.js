57f04132232adc50c0308c81eeb0bfeb841b0741
StrixVaria <zxqamf@gmail.com>
2014-10-14 23:02:43 -0400

    Ability to dynamically add rides and stores to the DOM.

--- game.js
+++ game.js
@@ -9,11 +9,21 @@ var theme_park = {
   stores: [],
   init: function(){
     window.timer = setInterval(function(){theme_park.tick()},1000);
+    this.addItemType('Haunted House', 'hauntedhouses', 'rides');
+    this.addItemType('Burger Joint', 'burgers', 'stores');
   },
   tick: function(){
     this.money += 1;
     $('#visitors').html(this.visitors);
-	$('#money').html(this.money);
+    $('#money').html(this.money);
+  },
+  addItemType: function(name, dom_id, type){
+    // TODO: Look up info rather than hard-code.
+    var box = $('<div/>', {id: dom_id, 'class': 'item-box'});
+    box.append($('<div/>', {'class': 'item-name', text: name}));
+    $('#' + type).append(box);
   }
 };
-$(document).ready(theme_park.init());
+$(document).ready(function(){
+  theme_park.init()
+});
